84d901be2ce1d3a25aa5368862ac4eeec0918469
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2025-11-05 17:02:51 -0500

    username in textfield is saved

--- app/src/main/java/edu/farmingdale/datastoresimplestoredemo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/datastoresimplestoredemo/MainActivity.kt
@@ -1,6 +1,5 @@
 package edu.farmingdale.datastoresimplestoredemo
 import android.content.Context
-import java.io.PrintWriter
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -9,19 +8,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text2.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import edu.farmingdale.datastoresimplestoredemo.data.AppPreferences
 import edu.farmingdale.datastoresimplestoredemo.ui.theme.DataStoreSimpleStoreDemoTheme
 import kotlinx.coroutines.launch
 import java.io.FileOutputStream
+import java.io.PrintWriter
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,12 +77,16 @@ fun DataStoreDemo(modifier: Modifier) {
     val store = AppStorage(LocalContext.current)
     val appPrefs = store.appPreferenceFlow.collectAsState(AppPreferences())
     val coroutineScope = rememberCoroutineScope()
+    var username by remember {mutableStateOf("Username")}
     Column (modifier = Modifier.padding(50.dp)) {
         Text("Values = ${appPrefs.value.userName}, " +
                 "${appPrefs.value.highScore}, ${appPrefs.value.darkMode}")
+        TextField(
+            value = username, onValueChange = {username = it}
+        )
         Button(onClick = {
             coroutineScope.launch {
-                store.saveUsername("somevaluehere")
+                store.saveUsername(username = username)
                 store.saveHighscore(24)
                 store.saveDarkModePref(false)
             }
@@ -84,9 +97,17 @@ fun DataStoreDemo(modifier: Modifier) {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun DataStoreDemoPreview() {
+    DataStoreSimpleStoreDemoTheme() {
+        DataStoreDemo(modifier = Modifier)
+    }
+}
+
 // ToDone 1: Modify the App to store a high score and a dark mode preference
-// ToDo 2: Modify the APP to store the username through a text field
-// ToDo 3: Modify the App to save the username when the button is clicked
-// ToDo 4: Modify the App to display the values stored in the DataStore
+// ToDone 2: Modify the APP to store the username through a text field
+// ToDone 3: Modify the App to save the username when the button is clicked
+// ToDone 4: Modify the App to display the values stored in the DataStore
 
 
